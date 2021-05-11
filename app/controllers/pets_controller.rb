2090aa4af238bd3e8a39e04a8f03568784ec5556
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-11 20:02:54 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -39,6 +39,7 @@ class PetsController < ApplicationController
     binding.pry
     @pet.update(name: params["pet_name"])
     if !params["owner_name"].empty?
+      
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
     redirect to "pets/#{@pet.id}"
