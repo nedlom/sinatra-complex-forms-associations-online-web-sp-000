d7d51173eb86f35a5eb848abf3ad9bc7d4cd0e51
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-11 20:02:06 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -38,6 +38,7 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     binding.pry
     @pet.update(name: params["pet_name"])
+    
     redirect to "pets/#{@pet.id}"
   end
 end
